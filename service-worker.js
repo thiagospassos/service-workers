44d95d180780447f153baa9591489c9a661dbba5
Thiago Passos <Thiago Passos>
2017-10-13 21:30:26 +1000

    init

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+//self.importScripts('./util/util.js')
+
+self.addEventListener('install', function (event) {
+    console.log('Install event');
+
+    event.waitUntil(self.skipWaiting());
+});
+
+
+self.addEventListener('fetch', function (event) {
+    console.log(`FETCH: ${event.request.url}`);
+
+    if(event.request.url.endsWith("png")){
+        event.respondWith(fetch('/images/ie.png'));
+    }
+});
