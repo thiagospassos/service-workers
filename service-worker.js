6db8c535518e7578755a9383058291eb5075cb0c
Thiago Passos <Thiago Passos>
2017-10-13 22:16:49 +1000

    sending cache example

--- service-worker.js
+++ service-worker.js
@@ -3,14 +3,30 @@
 self.addEventListener('install', function (event) {
     console.log('Install event');
 
-    event.waitUntil(self.skipWaiting());
+
+    event.waitUntil(_ => self.skipWaiting());
 });
 
 
 self.addEventListener('fetch', function (event) {
     console.log(`FETCH: ${event.request.url}`);
 
-    if(event.request.url.endsWith("png")){
-        event.respondWith(fetch('./images/ie.png'));
-    }
+    //prank
+    // if(event.request.url.endsWith("png")){
+    //     event.respondWith(fetch('./images/chrome.png'));
+    // }
+
+    //cache 1
+    event.respondWith(fetch(event.request)
+        .then(networkResponse => {
+            console.log(`WORKER: Updating cache: ${event.request.url}`);
+            var clone = networkResponse.clone();
+            caches.open('mycache').then(cache => cache.put(event.request, clone));
+            return networkResponse;
+        })
+        .catch(_ => {
+            console.log(`WORKER: Serving from cache: ${event.request.url}`);
+            return caches.open('mycache').then(cache => cache.match(event.request));
+        })
+    );
 });
